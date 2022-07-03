5965f188a1a28aad854efb9381bb558a3680f0ac
Tyler Darsey <bulldogboy912000@yahoo.com>
2022-07-03 14:29:27 -0500

    Update constants.ts
    Fix whitty's twitch

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -21,7 +21,7 @@ export const RESIDENT_DJS: { [key: string]: Dj } = {
     },
     "whitty": {
         name: "whitty",
-        twitch_url: "https://www.twitch.com/wittykat",
+        twitch_url: "https://www.twitch.com/whittykat",
     },
     "bleatr": {
         name: "bleatr",
