1910f4e362c3b692c1b15671ead163e8b11fc328
Tyler Darsey <bulldogboy912000@gmail.com>
2024-04-14 12:50:23 -0500

    Remove strapi

--- s4-strapi/src/api/dj/routes/dj.ts
+++
@@ -1,7 +0,0 @@
-/**
- * dj router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::dj.dj');
--- s4-strapi/src/api/dj/services/dj.ts
+++
@@ -1,7 +0,0 @@
-/**
- * dj service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::dj.dj');
