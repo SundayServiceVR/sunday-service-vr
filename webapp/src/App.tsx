52b1312a58be8876289ff24b5f6be3e9e81c0c6d
Tyler Darsey <bulldogboy912000@yahoo.com>
2024-04-17 20:15:00 -0500

    Clean unused routes

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -9,10 +9,8 @@ import Layout from './features/layout/Layout';
 import AnonymousLayout from './features/layout/AnonymousLayout';
 import Scheduler from './features/event/Scheduler';
 import ResetPassword from './features/auth/ResetPassword';
-import Whiteboard from './features/whiteboard/Whiteboard';
 
 import './App.css';
-import WhiteboardLayout from './features/whiteboard/WhiteboardLayout';
 
 function App() {
   return (
@@ -26,9 +24,6 @@ function App() {
             } 
           >
             <Route index element={<Scheduler />}/>
-            <Route path="/whiteboard" element={<WhiteboardLayout />}>
-              <Route index element={<Whiteboard />} />
-            </Route>
           </Route>
           <Route path="/login" element={ <AnonymousLayout><Login /></AnonymousLayout>} /> 
           <Route path="/resetPassword" element={ <AnonymousLayout><ResetPassword /></AnonymousLayout>} />
