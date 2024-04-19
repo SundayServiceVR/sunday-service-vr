32b39fef04f42191155f2dc6cc717375aaf68849
Tyler.Darsey@coxautoinc.com <Tyler.Darsey@coxautoinc.com>
2024-04-19 16:07:25 -0500

    Fix linting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,31 +9,17 @@
 
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
-
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
 initializeApp();
 
 export const whiteboard = onRequest(async (request, response) => {
-  const docRef = await getFirestore().collection("whiteboards").doc("current")
+  const docRef = await getFirestore().collection("whiteboards").doc("current");
   const document = await docRef.get();
   const whiteboard = document.data();
 
-  const key = request.query.path?.toString() ?? null;
-
-  let responseText;
-  if(key) {
-    responseText = whiteboard[key]?.toString();
-    if(!responseText)
-      response.statusCode = 500;
-      response.statusMessage = "Key not found. UwU";
-    
-  } else {
-    responseText = JSON.stringify(whiteboard);
-  }
+  const responseText = JSON.stringify(whiteboard);
 
   logger.info("Request from ", { structuredData: true });
   response.send(responseText);
