5d10b9e8d3d312ffb5e7abf3787c89886952e792
frosty <120986721+icedog225@users.noreply.github.com>
2024-07-07 10:43:01 -0700

    Add pages for opening spreadsheet and verifying lineup (#81)
    * Added signup sheet footer to Event Create page
    
    * Modified text for signup sheet info
    
    * Added descriptive text to whiteboard page
    
    * Improved signup sheet message
    
    * Added TBA fallback if empty host
    
    * Add firebase export to webapp gitignore
    
    * Added proposedineup message
    
    * Adjusted signup sheet section styling
    
    * Added Verify DJs to navigation
    
    * Populated verify DJs section
    
    * removed unused variable
    
    * Updated to Q3 signup sheet
    
    * Added descriptive text to verify DJs page
    
    * Changed header for announcement section
    
    * Modified pastes to use role IDs for pinging
    
    * Added attendee role to expected event test
    
    * Using MessagePasteCard in EventDetails
    
    * Removed unused imports

---
+++ webapp/src/util/constants.ts
@@ -0,0 +1,6 @@
+// Current URL: Q3 2024
+export const signupSheetUrl = "https://docs.google.com/spreadsheets/d/1fExUQrOqo8lP2AS35Atlk8E5TiTH6y4qVMPxlN1diTA/edit?usp=sharing`";
+
+// Discord roles
+export const performerRoleId = "1004491516359749722";
+export const attendeeRoleId = "1004491711856251061";
