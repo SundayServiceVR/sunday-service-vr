9442f88ea803197382233a34b23c510ae7bf2161
Tyler Darsey <bulldogboy912000@yahoo.com>
2024-09-08 11:13:28 -0500

    Remove redundant method

--- webapp/src/store/dj.ts
+++ webapp/src/store/dj.ts
@@ -6,10 +6,6 @@ export const createDj = async (dj: Dj) => {
     return await addDoc(collection(db, "djs"), dj);
 }
 
-export const saveDj = async (dj: Dj) => {
-    return await addDoc(collection(db, "djs"), dj);
-}
-
 export const updateDj = async (djId: string, dj: Dj) => {
     const newDoc = doc(db, "djs", djId);
     return await setDoc(newDoc, dj);
