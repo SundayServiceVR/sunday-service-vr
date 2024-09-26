b2e8d5fd2d0bd54c9d5b0f8e736ed7495373ecdb
Tyler Darsey <bulldogboy912000@yahoo.com>
2024-09-26 12:20:47 -0500

    Show slot names on the past event list. (#108)
    Co-authored-by: Tyler.Darsey@coxautoinc.com <Tyler.Darsey@coxautoinc.com>

--- webapp/src/features/event/EventList.tsx
+++ webapp/src/features/event/EventList.tsx
@@ -62,7 +62,7 @@ const EventList = ({ past = false}: Props) => {
                         <th>Date</th>
                         <th>Name</th>
                         <th>Host</th>
-                        <th>Number of Slots</th>
+                        <th>Slots</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -70,7 +70,7 @@ const EventList = ({ past = false}: Props) => {
                         <td><Link to={`/events/${event.id}`}>{event.start_datetime.toLocaleDateString()}</Link></td>
                         <td>{event.name}</td>
                         <td>{event.host}</td>
-                        <td>{event.slots.length}</td>
+                        <td>{event.slots.map(slot => slot.dj_name).join(", ")}</td>
                     </tr>)}
                 </tbody>
             </Table>
