cf6c4776e721a99660a0867a7ff2c4ef88319e2c
Tyler.Darsey@coxautoinc.com <Tyler.Darsey@coxautoinc.com>
2024-10-28 17:31:12 -0500

    Bump Signup Url

--- webapp/src/util/constants.ts
+++ webapp/src/util/constants.ts
@@ -1,5 +1,5 @@
 // Current URL: Q3 2024
-export const signupSheetUrl = "https://docs.google.com/spreadsheets/d/1fExUQrOqo8lP2AS35Atlk8E5TiTH6y4qVMPxlN1diTA/edit?usp=sharing`";
+export const signupSheetUrl = "https://docs.google.com/spreadsheets/d/1sj2M4Sn_zlDV2wzVjnkYiY6zmcuVR_GBhLI_i1BJZtE/edit?usp=sharing`";
 
 // Discord roles
 export const performerRoleId = "1004491516359749722";
