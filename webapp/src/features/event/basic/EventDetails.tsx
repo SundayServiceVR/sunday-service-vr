de6d564d074ef56b423f4fac192789d67d11dc38
Tyler Darsey <bulldogboy912000@yahoo.com>
2025-03-30 11:18:08 -0500

    Pass the signup sheet url into our signup sheet message. (#127)
    Co-authored-by: Tyler.Darsey@coxautoinc.com <Tyler.Darsey@coxautoinc.com>

--- webapp/src/features/event/basic/EventDetails.tsx
+++ webapp/src/features/event/basic/EventDetails.tsx
@@ -4,11 +4,36 @@ import "react-datepicker/dist/react-datepicker.css";
 import { Card, CardBody } from "react-bootstrap";
 import { getSignupsPostedMessage } from "../../../util/messageWriters";
 import MessagePasteCard from "../messaging/MessagePasteCard";
+import { doc, getDoc, getFirestore } from "@firebase/firestore";
+import { useEffect, useState } from "react";
+import Spinner from "../../../components/spinner";
 
 const EventDetails = () => {
 
+    const [signupSheetUrl, setSignupSheetUrl] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchSignupUrl = async () => {
+            const firestore = getFirestore();
+            const docRef = doc(firestore, "/global/global_settings");
+            try {
+                const docSnap = await getDoc(docRef);
+                const url = docSnap.exists() ? docSnap.data().signup_url : "unable to find signup url";
+                setSignupSheetUrl(url);
+            } catch (error) {
+                console.error("Error fetching signup URL:", error);
+                setSignupSheetUrl("unable to find signup url");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchSignupUrl();
+    }, []);
+
     const [eventScratchpad, proposeEventChange] = useEventOperations();
-    const message = getSignupsPostedMessage(eventScratchpad);
+    const message = getSignupsPostedMessage(eventScratchpad, signupSheetUrl);
 
     return <>
         <Card>
@@ -19,18 +44,21 @@ const EventDetails = () => {
         </Card>
         <Card className="mt-4">
             <CardBody>
-                <div>
-                    <h2 className="display-6">Signup Sheet</h2>
-                    <p>At the start of the week (usually Monday), unlock the S4 signup spreadsheet and post a link to it in <a target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://discord.com/channels/1004489038159413248/1204320477732929566">
-                            #scheduling
-                    </a> in Discord.</p>
-                    <p>
-                        Here's a sample message to get you started.
-                    </p>
-                    <MessagePasteCard message={message}></MessagePasteCard>
-                </div>
+                { loading ? <Spinner type="simple" /> : 
+                    <div>
+                        <h2 className="display-6">Signup Sheet</h2>
+                        <p>At the start of the week (usually Monday), unlock the S4 signup spreadsheet and post a link to it in <a target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://discord.com/channels/1004489038159413248/1204320477732929566">
+                                #scheduling
+                        </a> in Discord.</p>
+                        <p>
+                            Here's a sample message to get you started.
+                        </p>
+                        <MessagePasteCard message={message}></MessagePasteCard>
+                    </div>
+                }
+               
             </CardBody>
         </Card>
     </>
