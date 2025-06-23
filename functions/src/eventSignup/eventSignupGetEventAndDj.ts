831d617358665350fe203010a9cac5dad927c426
Tyler Darsey <tyler.darsey@coxautoinc.com>
2025-06-23 10:13:01 -0500

    Enable cors

--- functions/src/eventSignup/eventSignupGetEventAndDj.ts
+++ functions/src/eventSignup/eventSignupGetEventAndDj.ts
@@ -6,40 +6,49 @@ import { docToEvent } from "../../../webapp/src/store/converters";
 
 const db = admin.firestore();
 
-export const eventSignupGetEventAndDj = functions.https.onRequest(async (req, res) => {
-    allowMethod(req, res, "GET");
-    const { discord_id } = await authenticate(req, res);
+export const eventSignupGetEventAndDj = functions.https.onRequest(
+    {
+        cors: [
+            "localhost",
+            /^https:\/\/sunday-service-vr-[^.]+\.web\.app$/,
+            "https://sunday-service-vr.web.app",
+            "https://s4tan.s4vr.net",
+        ],
+    },
+    async (req, res) => {
+        allowMethod(req, res, "GET");
+        const { discord_id } = await authenticate(req, res);
 
-    const { event_id } = req.query;
-    if (!event_id) {
-        res.status(400).send("Missing event_id parameter");
-        return;
+        const { event_id } = req.query;
+        if (!event_id) {
+            res.status(400).send("Missing event_id parameter");
+            return;
+        }
+
+        // Fetch Event record
+        const eventSnap = await db.collection("events").doc(event_id as string).get();
+        if (!eventSnap.exists) {
+            res.status(404).send("Event not found");
+            return;
+        }
+
+        // Fetch DJ record
+        const djSnap = await db.collection("djs").where("discord_id", "==", discord_id).limit(1).get();
+        if (djSnap.empty) {
+            res.status(404).send("DJ record not found");
+            return;
+        }
+
+        const sanitizedEvent = docToEvent(eventSnap);
+
+        // Remove signups that do not match the DJ
+        sanitizedEvent.signups = sanitizedEvent.signups.filter((signup) => {
+            return signup.dj_refs.some((ref) => ref.path === djSnap.docs[0].ref.path);
+        });
+
+        res.send({
+            event: sanitizedEvent,
+            dj: djSnap.docs[0].data(),
+        });
     }
-
-
-    // Fetch Event record
-    const eventSnap = await db.collection("events").doc(event_id as string).get();
-    if (!eventSnap.exists) {
-        res.status(404).send("Event not found");
-        return;
-    }
-
-    // Fetch DJ record
-    const djSnap = await db.collection("djs").where("discord_id", "==", discord_id).limit(1).get();
-    if (djSnap.empty) {
-        res.status(404).send("DJ record not found");
-        return;
-    }
-
-    const sanitizedEvent = docToEvent(eventSnap);
-
-    // Remove signups that do not match the DJ
-    sanitizedEvent.signups = sanitizedEvent.signups.filter((signup) => {
-        return signup.dj_refs.some((ref) => ref.path === djSnap.docs[0].ref.path);
-    });
-
-    res.send({
-        event: sanitizedEvent,
-        dj: djSnap.docs[0].data(),
-    });
-});
+);
