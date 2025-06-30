6b5611e95a5c140544244be2318f3a7716f8ab2a
Tyler Darsey <tyler.darsey@coxautoinc.com>
2025-06-30 08:49:47 -0500

    Show/Hide message

--- webapp/src/features/event/basic/EventDetails.tsx
+++ webapp/src/features/event/basic/EventDetails.tsx
@@ -3,7 +3,7 @@ import EventBasicDetailsForm from "./EventBasicDetailsForm";
 import { Card, CardBody } from "react-bootstrap";
 import { getSignupsPostedMessage } from "../../../util/messageWriters";
 import MessagePasteCard from "../messaging/MessagePasteCard";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -21,22 +21,25 @@ const EventDetails = () => {
                 <EventBasicDetailsForm event={eventScratchpad} onEventChange={proposeEventChange} />
             </CardBody>
         </Card>
-        <Card className="mt-4">
-            <CardBody>
-                <div>
-                    <h2 className="display-6">Signup Sheet</h2>
-                    <p>At the start of the week (usually Monday), unlock the S4 signup spreadsheet and post a link to it in <a target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://discord.com/channels/1004489038159413248/1204320477732929566">
+        {eventScratchpad.signupsAreOpen &&
+            <Card className="mt-4">
+                <CardBody>
+                    <div>
+                        <h2 className="display-6">Signup Sheet</h2>
+                        <p>At the start of the week (usually Monday), unlock the S4 signup spreadsheet and post a link to it in <a target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://discord.com/channels/1004489038159413248/1204320477732929566">
                             #scheduling
-                    </a> in Discord.</p>
-                    <p>
-                        Here's a sample message to get you started.
-                    </p>
-                    <MessagePasteCard message={message}></MessagePasteCard>
-                </div>
-            </CardBody>
-        </Card>
+                        </a> in Discord.</p>
+                        <p>
+                            Here's a sample message to get you started.
+                        </p>
+                        <MessagePasteCard message={message}></MessagePasteCard>
+                    </div>
+                </CardBody>
+            </Card>
+        }
+
     </>
 };
 
