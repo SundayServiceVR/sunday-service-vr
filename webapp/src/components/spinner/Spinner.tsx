c8eeb5d0c28f6cff2a9254d0ffa9a7754787c4c5
Tyler Darsey <bulldogboy912000@yahoo.com>
2025-07-11 17:48:47 -0500

    Auth hotfix offer logout (#141)
    * Improve logging
    
    * Offer a logout link
    
    * Stand up hotfix
    
    ---------
    
    Co-authored-by: Tyler Darsey <tyler.darsey@coxautoinc.com>

--- webapp/src/components/spinner/Spinner.tsx
+++ webapp/src/components/spinner/Spinner.tsx
@@ -1,4 +1,7 @@
+import { useState, useEffect } from "react";
 import { Spinner as BootstrapSpinner } from "react-bootstrap";
+import { signOut } from "firebase/auth";
+import { auth } from "../../util/firebase";
 
 import S4Logo from "../../assets/svg/S4_Logo.svg?react";
 
@@ -8,11 +11,38 @@ type SpinnerProps = {
   type?: "simple"|"logo"
 }
 const Spinner = ({ type = "simple" }: SpinnerProps) => {
+  const [showLogoutLink, setShowLogoutLink] = useState(false);
+
+  useEffect(() => {
+    if (type === "logo") {
+      const timer = setTimeout(() => {
+        setShowLogoutLink(true);
+      }, 10000); // 10 seconds
+
+      return () => clearTimeout(timer);
+    } else {
+      // Reset the logout link state if type is not logo
+      setShowLogoutLink(false);
+    }
+  }, [type]);
+
+  const handleLogout = () => {
+    signOut(auth);
+  };
+
   switch (type) {
     case "logo": {
       return (
         <div className="spinnerWrapper">
           <S4Logo className="logoSpinner withFade" />
+          {showLogoutLink && (
+            <div className="logoutMessage">
+              <p>Looks like this is taking a while: This is a hotfix for some issue's we've been seeing. If you are stuck, log out and log back in.</p>
+              <button onClick={handleLogout} className="btn btn-link">
+                Log out
+              </button>
+            </div>
+          )}
         </div>
       );
     }
