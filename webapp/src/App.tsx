9fe9de8978490708b2cd89e49c65edb287680d54
Tyler Darsey <bulldogboy912000@yahoo.com>
2025-07-11 07:42:00 -0500

    Add layout to signups (#138)
    * Set up a "User Info" section
    
    * Little cleaning
    
    * Remove "Global Settings" stuff that's not needed anymore.
    
    * Remove more of global settings.
    
    * Tweaks
    
    * Add the stream link to signups.
    
    * Remove "stream/technical" details
    
    * Fix Errors
    
    ---------
    
    Co-authored-by: Tyler Darsey <tyler.darsey@coxautoinc.com>

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -16,7 +16,7 @@ import DjList from './features/dj/DjList';
 import { Toaster } from 'react-hot-toast';
 import { DiscordIdInfo } from './features/dj/discordIdInfo/DiscordIdInfo';
 import { eventRoutes } from './features/event/routes';
-import GlobalSettings from "./features/globalSettings/GlobalSettings";
+import { UserInfo } from './features/user/UserInfo';
 
 import { EventDjPlayMapperProvider } from './contexts/useEventDjCache/eventDjCacheProvider';
 
@@ -89,22 +89,22 @@ function App() {
           element: <DiscordIdInfo />
         },
         {
-          path: "globalSettings",
-          element: <GlobalSettings />
+          path: "userInfo",
+          element: <UserInfo />
         },
       ],
     },
     {
       path: "eventSignup",
+      element: <FirebaseAuthProvider>
+            <RoleGuard requireAnyRole={['dj', 'host']}><Layout /></RoleGuard>
+      </FirebaseAuthProvider>,
       children: [
         {
           path: `:eventId`,
           // TODO: Refactor so we don't have two FirebaseAuthProviders
-          element: <FirebaseAuthProvider>
-            <RoleGuard requireAnyRole={['dj', 'host']}>
-              <EventSignupRoot />
-            </RoleGuard>
-          </FirebaseAuthProvider>,
+          element: 
+              <EventSignupRoot />,
 
           children: [
             {
@@ -116,6 +116,18 @@ function App() {
             }
  
           ]
+        },
+      ]
+    },
+    {
+      path: "/userInfo",
+      element: <FirebaseAuthProvider>
+            <RoleGuard requireAnyRole={['dj', 'host', 'admin']}><Layout /></RoleGuard>
+      </FirebaseAuthProvider>,
+      children: [
+        {
+          index: true,
+          element: <UserInfo />
         },
       ]
     },
